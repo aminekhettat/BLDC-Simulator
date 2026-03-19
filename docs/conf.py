6bce6ff7bc4de31ddb6b23dcd10e23bcea2b79d9
aminekhettat <amine.khettat@blindsystems.org>
2026-03-19 18:50:14 +0100

    Clean Sphinx warning configuration

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,6 @@ html_static_path = ["_static"]
 
 html_theme_options = {
     "logo_only": False,
-    "display_version": True,
     "prev_next_buttons_location": "bottom",
     "style_external_links": False,
     "vcs_pageview_mode": "",
